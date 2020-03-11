8ad326610a5ede366c47d497bba9001319dd644d
MathijsD1 <deelmanmathijs@gmail.com>
2020-03-11 09:25:29 +0100

    Add sprite to Actor

--- common/actor.cpp
+++ common/actor.cpp
@@ -1,1 +1,6 @@
 #include <common/actor.h>
+
+void Actor::addSprite(Sprite* sprite) 
+{
+	this->_sprite = sprite;
+}
--- common/actor.h
+++ common/actor.h
@@ -1,9 +1,14 @@
 #ifndef ACTOR_H
 #define ACTOR_H
 
+#include <common/sprite.h>
+
 class Actor
 {
-
+public:
+	void addSprite(Sprite* sprite);
+private:
+	Sprite* _sprite;
 };
 
 #endif
