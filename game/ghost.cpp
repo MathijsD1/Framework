c56bb8ef51987fc7d95d636ab871943e670de002
MathijsD1 <deelmanmathijs@gmail.com>
2020-03-31 08:18:57 +0200

    Add ghost movement towards a given vector2

--- game/ghost.cpp
+++ game/ghost.cpp
@@ -7,11 +7,19 @@ Ghost::Ghost()
 
 	this->position = Vector2(SCREENWIDTH / 2, SCREENHEIGHT / 2);
 	this->scale = Vector2(1, 1);
-	this->rotation = 30;
+	this->rotation = 0;
 }
 
 void Ghost::update() {
-	this->scale += Vector2(0.1f, 0.1f) * Time::deltaTime();
-	
+
 	Debug::printMessage("Ghost Update");
+	moveTowards(Vector2(255, 125));
+}
+
+void Ghost::moveTowards(Vector2 target) 
+{
+	Vector2 direction = target - this->position;
+	direction.normalize();
+
+	this->position += direction * 100 * Time::deltaTime();
 }
--- game/ghost.h
+++ game/ghost.h
@@ -2,12 +2,14 @@
 #define GHOST_H
 
 #include <common/actor.h>
+#include <common/vectorx.h>
 
 class Ghost : public Actor
 {
 public:
 	Ghost();
 	virtual void update();
+	void moveTowards(Vector2 target);
 };
 
 #endif
